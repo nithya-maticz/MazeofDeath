5f04f194adb803b856ff9532cede3416e97a7b9a
kavinmaticz <kavinsrimaticz@gmail.com>
2025-07-25 17:33:53 +0530

    commit

--- Assets/Maze1/Maze_of_Death/Scripts/Game_Manager.cs
+++ Assets/Maze1/Maze_of_Death/Scripts/Game_Manager.cs
@@ -247,6 +247,7 @@ public class Game_Manager : MonoBehaviour
                 currentBullets--;
                 Debug.Log("Shot fired! Bullets left: " + currentBullets);
                 UpdateUI();
+                PlayerMovements.Instance._Animator.SetTrigger("Shoot");
                 Bullet bullet = Instantiate(BulletPrefab, BulletSpawner);
                 bullet.transform.localPosition = Vector3.zero;
                 bullet.Target = PlayerMovements.Instance.ManualTarget;
@@ -280,6 +281,7 @@ public class Game_Manager : MonoBehaviour
                     currentBullets--;
                     Debug.Log("Shot fired! Bullets left: " + currentBullets);
                     UpdateUI();
+                    PlayerMovements.Instance._Animator.SetTrigger("Shoot");
                     Bullet bullet = Instantiate(BulletPrefab, BulletSpawner);
                     bullet.transform.localPosition = Vector3.zero;
                     bullet.Target = AutoAim.currentTarget.transform;
