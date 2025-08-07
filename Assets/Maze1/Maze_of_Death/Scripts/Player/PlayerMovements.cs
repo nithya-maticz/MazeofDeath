696f60e59238396adee523421a23d0c57ad97a56
nithya-maticz <nithyamaticz@gmail.com>
2025-08-07 09:47:21 +0530

    commit

--- Assets/Maze1/Maze_of_Death/Scripts/Player/PlayerMovements.cs
+++ Assets/Maze1/Maze_of_Death/Scripts/Player/PlayerMovements.cs
@@ -185,13 +185,13 @@ public class PlayerMovements : MonoBehaviour
             _Animator.SetTrigger("GunIdle");
             wasIdle = true;
         }
-        else if (isGun && isWalking && (!wasWalking || !wasGun))
+        else if (isGun && isWalking && (!wasWalking || !wasGun) && !isAttack)
         {
             ResetAllTriggers();
             _Animator.SetTrigger("WalkWithGun");
             wasIdle = false;
         }
-        else if (!isGun && isWalking && (!wasWalking || wasGun))
+        else if (!isGun && isWalking && (!wasWalking || wasGun) && !isAttack)
         {
             ResetAllTriggers();
             _Animator.SetTrigger("Walk");
