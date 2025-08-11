89aad53dde179ef153fa9cd2c51199e261257a8b
nithya-maticz <nithyamaticz@gmail.com>
2025-08-11 18:02:53 +0530

    commit

--- Assets/Maze1/Maze_of_Death/Scripts/TileMap LevelEditor/LevelManager.cs
+++ Assets/Maze1/Maze_of_Death/Scripts/TileMap LevelEditor/LevelManager.cs
@@ -35,7 +35,7 @@ public class LevelManager : MonoBehaviour
 
     void Start()
     {
-        levelToLoad = 3;
+        levelToLoad = 4;
         LoadLevelData();
         SpawnTiles();
         SpawnBackgrounds();
