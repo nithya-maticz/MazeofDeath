996c0d217720d84a3f55662f5395e9e79a0177f7
kavinmaticz <kavinsrimaticz@gmail.com>
2025-08-22 16:40:59 +0530

    COMMIT

--- Assets/Maze1/Maze_of_Death/Scripts/Asset Downloader and API/ApiManager.cs
+++ Assets/Maze1/Maze_of_Death/Scripts/Asset Downloader and API/ApiManager.cs
@@ -19,7 +19,7 @@ public class ApiManager : MonoBehaviour
     }
     void Start()
     {
-       // LobbyManager.currentLevel = 6;
+        //LobbyManager.currentLevel = 13;
         GetLevels();
     }
 
