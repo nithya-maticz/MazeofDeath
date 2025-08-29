daa9f7416a4eeb73ccd62a0aa6fca42b6e94b525
kavinmaticz <kavinsrimaticz@gmail.com>
2025-08-29 12:25:24 +0530

    comit

---
+++ Assets/Maze1/Maze_of_Death/Scripts/Tanker.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Tanker : MonoBehaviour
+{
+    public Animator animator;
+    public int health = 3;
+    public int maxHealth = 3;
+    public GameObject attributes;
+    public Image fillImage;
+    public GameObject tank;
+    public GameObject blast;
+    public CircleCollider2D colloider;
+    public TankBlaster radius;
+
+    void Start()
+    {
+        UpdateHealth();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Bullet"))
+        {
+            Destroy(collision.gameObject);
+
+            if (!attributes.activeSelf)
+            {
+                attributes.SetActive(true);
+                animator.SetTrigger("Radius");
+            }
+                
+
+            health--;
+            UpdateHealth();
+        }
+    }
+
+    void UpdateHealth()
+    {
+        health = Mathf.Clamp(health, 0, maxHealth);
+
+        fillImage.fillAmount = (float)health / maxHealth;
+
+        if (fillImage.fillAmount <= 0f)
+        {
+           attributes.SetActive(false);
+            colloider.enabled = false;
+            Destroy(tank);
+
+            blast.SetActive(true);
+            animator.SetTrigger("Blast");
+        }
+        else
+        {
+            animator.SetTrigger("Shoot");
+        }
+    }
+}
